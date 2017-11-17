d71c0261c805703de8bcc63186c00d3f0417a2fb
Anshul Kapoor <anshulkapoor018@gmail.com>
2017-11-17 20:10:04 +0530

    Testing delete

--- server/server.js
+++ server/server.js
@@ -11,7 +11,6 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-// Todos
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
@@ -35,76 +34,41 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
-  if(!ObjectID.isValid(id)){
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
   Todo.findById(id).then((todo) => {
-    if(!todo){
+    if (!todo) {
       return res.status(404).send();
     }
+
     res.send({todo});
   }).catch((e) => {
     res.status(400).send();
-  });
-});
-
-// USERS
-app.post('/users', (req, res) => {
-  var user = new User({
-    email: req.body.email
-  });
-
-  user.save().then((doc) => {
-    res.send(doc);
-  }, (e) => {
-    res.status(400).send(e);
-  });
-});
-
-app.get('/users', (req, res) => {
-  User.find().then((users) => {
-    res.send({users});
-  }, (e) => {
-    res.status(400).send(e);
   });
 });
 
 app.delete('/todos/:id', (req, res) => {
   var id = req.params.id;
 
-  if(!ObjectID.isValid(id)){
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
   Todo.findByIdAndRemove(id).then((todo) => {
-    if(!todo){
+    if (!todo) {
       return res.status(404).send();
     }
+
     res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.listen(port, () => {
-  console.log(`Started on port ${port}`);
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
--- server/test/server.test.js
+++ server/test/server.test.js
@@ -7,15 +7,15 @@ const {Todo} = require('./../models/todo');
 
 const todos = [{
   _id: new ObjectID(),
-  text: 'First'
+  text: 'First test todo'
 }, {
   _id: new ObjectID(),
-  text: 'Second'
+  text: 'Second test todo'
 }];
 
 beforeEach((done) => {
   Todo.remove({}).then(() => {
-    Todo.insertMany(todos);
+    return Todo.insertMany(todos);
   }).then(() => done());
 });
 
@@ -76,7 +76,7 @@ describe('GET /todos', () => {
 describe('GET /todos/:id', () => {
   it('should return todo doc', (done) => {
     request(app)
-      .get(`/todos/${todos[0]._id.toHexString() }`)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
       .expect(200)
       .expect((res) => {
         expect(res.body.todo.text).toBe(todos[0].text);
@@ -84,19 +84,57 @@ describe('GET /todos/:id', () => {
       .end(done);
   });
 
-  it('should return 404 for no todo docs', (done) => {
+  it('should return 404 if todo not found', (done) => {
     var hexId = new ObjectID().toHexString();
+
     request(app)
       .get(`/todos/${hexId}`)
-      .send({})
       .expect(404)
       .end(done);
   });
 
-  it('should return 404 for non ObjectID docs', (done) => {
+  it('should return 404 for non-object ids', (done) => {
     request(app)
-      .get(`/todos/123abc`)
-      .send({})
+      .get('/todos/123abc')
+      .expect(404)
+      .end(done);
+  });
+});
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if object id is invalid', (done) => {
+    request(app)
+      .delete('/todos/123abc')
       .expect(404)
       .end(done);
   });
